b74d88bfed222ba8ebde01cae7b7391ecb3e8d07
Betul <betulkoru06@gmail.com>
2024-01-18 22:49:14 +0300

    products arr and category methods

--- app.js
+++ app.js
@@ -2,3 +2,33 @@ const btnDivs = document.getElementById("btns")
 const productDivs = document.getElementById("products")
 const searchInput = document.getElementById("searchInput")
 const categoryTitle = document.getElementById("category")
+
+//API URLS
+// - Tüm ürünler için => https://anthonyfs.pythonanywhere.com/api/products/
+// - Tek bir ürün için = https://anthonyfs.pythonanywhere.com/api/products/{productID}
+
+const products = []
+
+const getProducts = async () => {
+    const res = await fetch ("https://anthonyfs.pythonanywhere.com/api/products/")
+    const data = await res.json()
+    products = data
+}
+
+getProducts()
+
+const category = () => {
+    console.log(products);
+    // const categoryArr = products.map(item => item.category);
+    // console.log(categoryArr);
+    //* 1st method
+    // let categoryArr = ["all"]
+    // products.forEach(item => {
+    //     if(!categoryArr.includes(item.category)){
+    //         categoryArr.push(item.category)
+    //     }
+    // })
+    //* 2nd method
+    const categoryArr = products.reduce(() => {}, ["all"])
+
+}
