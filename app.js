0eb189b2d1de7108365acb810cbb1d1ac8bc9131
Betul <betulkoru06@gmail.com>
2024-01-18 22:26:36 +0300

    variables

---
+++ app.js
@@ -0,0 +1,4 @@
+const btnDivs = document.getElementById("btns")
+const productDivs = document.getElementById("products")
+const searchInput = document.getElementById("searchInput")
+const categoryTitle = document.getElementById("category")
