0771708e5e6a273472c1af7bacc7561b535a097e
Betul <betulkoru06@gmail.com>
2024-01-19 20:41:13 +0300

    productDiv innerHTML

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ const getProducts = async () => {
     const res = await fetch ("https://anthonyfs.pythonanywhere.com/api/products/")
     const data = await res.json()
     products = data
+    category();
+    displayProducts(products);
 }
 
 getProducts()
@@ -58,3 +60,35 @@ const category = () => {
         btnDivs.appendChild(btn);
     })
 };
+
+function displayProducts(arr){
+    productDivs.innerHTML = "";
+    arr.forEach((item) => {
+        const { id, title, description, price, image } = item;
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("col");
+        productDiv.setAttribute("id", id);
+        productDiv.innerHTML = `
+            <div class="card">
+                <img src="${image}" class="p-2" height="250px" alt="...">
+                <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+                  <p class="card-text line-clamp-2">${description}</p>
+                </div>
+                <div class="card-footer w-100 fw-bold d-flex justify-content-between gap-3">
+                <span>Price:</span><span>${price} $</span>
+                    
+                </div>
+                <div class="card-footer w-100 d-flex justify-content-center gap-3">
+                    <button class="btn btn-danger">
+                    Sepete Ekle
+                    </button>
+                    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    See Details
+                    </button>
+                </div>
+              </div>
+            `;
+});
+       
+}
